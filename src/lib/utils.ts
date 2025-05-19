ca5d939c610680ac141da6f6cf4bbc9ebcefec55
Rafael Willen <rafaelpadre20@gmail.com>
2025-05-19 15:38:55 +0100

    feat: implement area  section

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function addHexOpacity(hexColor: string, opacityPercent = 100) {
+  const opacity = Math.round((opacityPercent / 100) * 255);
+  const hex = hexColor.replace("#", "");
+  return `#${hex}${opacity.toString(16).padStart(2, "0")}`;
 }
