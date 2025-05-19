f682ace7edc445a53c7e6f486620e4e60be2358c
Rafael Willen <rafaelpadre20@gmail.com>
2025-05-19 15:38:47 +0100

    feat: add company logos

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,4 +1,8 @@
 // SVG
 export { default as InstagramIcon } from "@/assets/icons/InstagramIcon.svg";
 export { default as LinkedInIcon } from "@/assets/icons/LinkedInIcon.svg";
+export { default as AngolaTodayLogo } from "@/assets/svg/angola-today-logo.svg";
+export { default as BoldEventsLogo } from "@/assets/svg/bold-events-logo.svg";
+export { default as BoldLabsLogo } from "@/assets/svg/bold-labs-logo.svg";
+export { default as LabutaLogo } from "@/assets/svg/labuta-logo.svg";
 export { default as LogoSVG } from "@/assets/svg/logo.svg";
